81897e8115ee721612ec6e3281b66394a361c140
Pedro Cavaleiro <pedro.cavaleiro@i-do.pt>
2025-05-15 14:59:31 +0100

    Removes notifier dependency

--- Package.swift
+++ Package.swift
@@ -17,16 +17,11 @@ let package = Package(
             name: "ExtendedSwift",
             targets: ["ExtendedSwift"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/PedroCavaleiro/Notifier", .upToNextMajor(from: "1.1.0")),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ExtendedSwift",
-            dependencies: ["Notifier"]
         ),
 
     ]
