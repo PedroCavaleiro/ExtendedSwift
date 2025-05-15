120f30972952b19f6fd89dea830e821aa35b057b
Pedro Cavaleiro <pedro.cavaleiro@i-do.pt>
2025-05-15 15:22:26 +0100

    Removes unsafe flags

--- Package.swift
+++ Package.swift
@@ -21,10 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ExtendedSwift",
-            swiftSettings: [
-                .unsafeFlags(["-swift-version", "5.9"])
-            ]
+            name: "ExtendedSwift"
         ),
 
     ]
